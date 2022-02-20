af2cfe1507a7da719d27802039055cd5d430af94
Satyabrata2000 <satyabrata2000@gmail.com>
2022-02-20 00:45:54 -0600

    Phase1Project

---
+++ src/phase1/AmazonSearch.java
@@ -0,0 +1,50 @@
+package phase1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class AmazonSearch {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Phase1Project\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MILLISECONDS);
+
+        WebElement search = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+        search.sendKeys("iphone12");
+        WebElement searchbtn = driver.findElement(By.xpath("//input[@id='nav-search-submit-button']"));
+        searchbtn.click();
+
+        List<WebElement> text = new ArrayList<>(driver.findElements(By.xpath("//span[@class ='a-size-medium a-color-base a-text-normal']")));
+        List<WebElement> price = new ArrayList<>(driver.findElements(By.xpath("//span[@class ='a-price-whole']")));
+
+        HashMap<String,String> values= new HashMap<String,String>();
+
+
+        System.out.println(text.size());
+        for (int i=0;i<=price.size()-1;i++) {
+            if (text.get(i).getText().contains("Apple iPhone 12")) {
+
+                values.put(text.get(i).getText(), price.get(i).getText());
+            }
+        }
+
+        for(Map.Entry<String, String> entry1 :values.entrySet()) {
+
+            System.out.println(entry1.getKey() + " - "+entry1.getValue());
+        }
+
+        driver.close();
+    }
+}
